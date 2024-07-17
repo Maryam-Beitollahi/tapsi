cce529d730511fb5b9edab093f8ec433b785baf2
Maryam-Beitollahi <maryambeitollahi71@gmail.com>
2024-07-17 17:59:31 +0330

    commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const toggler = document.querySelector(".nav__toggler");
+const navbar = document.querySelector(".nav");
+toggler.addEventListener("click", (e) => {
+    navbar.classList.toggle("nav__expanded");
+    });
+
+const tabs = document.querySelectorAll (".tab");
+const tabContents = document.querySelectorAll (".tab-content");
+tabs.forEach ((tab) => {...
+});
+
